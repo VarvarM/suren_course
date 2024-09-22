c1507d938b4ac4be050f1d4597d08ff06df59caf
VarvarM <miha.furtat@yandex.ru>
2024-09-22 21:48:46 +0300

    create user and post models

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,6 +1,8 @@
-__all__ = ("Base", "Product", "DatabaseHelper", "db_helper")
+__all__ = ("Base", "Post", "Product", "DatabaseHelper", "db_helper", "User")
 
 
 from .base import Base
 from .product import Product
 from .db_helper import DatabaseHelper, db_helper
+from .user import User
+from .post import Post
